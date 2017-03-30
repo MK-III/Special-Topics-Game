3a758bc68d474cfdcdf561378d27adfb6b3104c1
AndrewCrittenden <crittenden.andrew26@gmail.com>
2017-03-30 11:53:48 -0400

    mistake

--- Special Topics Game/Assets/Scenes/Enemies/Alien.cs
+++ Special Topics Game/Assets/Scenes/Enemies/Alien.cs
@@ -38,23 +38,22 @@ public class Alien : Enemy {
         {
         switch (Instantiaion.player.getUsedAbility())
         {
-                    case 1:
-                        usedAbility = 1;
-                        break;
                     case 2:
                         usedAbility = 3;
                         break;
                     case 3:
-                        usedAbility = 1;
+                        usedAbility = 2;
                         break;
                     case 4:
-                        usedAbility = 2;
+                        usedAbility = 4;
+                        healCount += 1;
                         break;
                     default:
                         usedAbility = 1;
                         break;
         }
         }
+        
         return usedAbility;
     }
     
